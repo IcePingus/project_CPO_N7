bfa0d3a409c1df013b8dabae55f76213fc3b677e
Alconis-github <ncombal@gmail.com>
2023-05-24 11:42:10 +0200

    US-106 : Fixed imports and renamed variable

--- src/graphic/view/ToolInternalFrame.java
+++ src/graphic/view/ToolInternalFrame.java
@@ -1,19 +1,16 @@
 package graphic.view;
 
-import graphic.model.*;
+import graphic.model.tools.*;
 
 import javax.swing.*;
-import javax.swing.border.Border;
-import javax.tools.Tool;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.net.URL;
 
 public class ToolInternalFrame extends JInternalFrame implements ActionListener {
 
     private Toolbox toolbox;
-    private int toolChoisi;
+    private int activeTool;
 
     public ToolInternalFrame() {
         super("Tools");
@@ -38,16 +35,16 @@ public class ToolInternalFrame extends JInternalFrame implements ActionListener 
             this.toolbox.getToolsButtons().get(i).setIcon(this.toolbox.getTools().get(i).getImage());
             this.toolbox.getToolsButtons().get(i).addActionListener(this);
         }
-        this.toolChoisi = 0;
+        this.activeTool = 0;
         this.toolbox.getToolsButtons().get(0).setBackground(Color.red);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         for (int i = 0; i < this.toolbox.getTools().size(); i++) {
-            if (e.getSource() == this.toolbox.getToolsButtons().get(i) && this.toolChoisi != i) {
-                this.toolbox.getToolsButtons().get(this.toolChoisi).setBackground(null);
-                this.toolChoisi = i;
+            if (e.getSource() == this.toolbox.getToolsButtons().get(i) && this.activeTool != i) {
+                this.toolbox.getToolsButtons().get(this.activeTool).setBackground(null);
+                this.activeTool = i;
                 this.toolbox.getToolsButtons().get(i).setBackground(Color.red);
             }
         }
