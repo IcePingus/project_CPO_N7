409dbb6f0127a6a31d6a5c4c1f84555ca67581ee
IcePingus <pingus32@hotmail.fr>
2023-06-12 11:11:15 +0200

    US-323 : fix squareroundshape and slider panel

--- src/graphic/model/tools/ToolCommand.java
+++ src/graphic/model/tools/ToolCommand.java
@@ -16,5 +16,7 @@ public interface ToolCommand extends Observer {
 
     boolean getHasShapeSelection();
 
+    boolean getIsSquareRoundShape();
+
     void execute(int oldX, int oldY, int currentX, int currentY, BufferedImage bufferedImage, Graphics2D graphics2D, int click, int size, boolean square, boolean isFirstPoint, JComponent canva);
 }
